ef0d05694614f36b60fbcfe3eebbf57a7338203a
rohithv97 <rohithv97@gmail.com>
2019-08-07 18:39:24 +0530

    App.Component.Html updated

--- my-first-app/src/app/server3/server3.component.ts
+++ my-first-app/src/app/server3/server3.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: '[app-server3]
-  selector: 'app-server3',
+  selector: '.app-server3',
+  //selector: 'app-server3',
   templateUrl: './server3.component.html',
   styleUrls: ['./server3.component.css']
 })
