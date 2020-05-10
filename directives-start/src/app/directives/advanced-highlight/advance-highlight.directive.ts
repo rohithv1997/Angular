88ede3c922e886acdc4e8d4176a0e836208a8bb4
RohithVenkataRamanaNanthan <rohith_venkatarama@srmuniv.edu.in>
2020-05-10 12:06:48 +0530

    structure directive updated

--- directives-start/src/app/directives/advanced-highlight/advance-highlight.directive.ts
+++ directives-start/src/app/directives/advanced-highlight/advance-highlight.directive.ts
@@ -8,6 +8,6 @@ export class AdvanceHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue', RendererStyleFlags2.Important);
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue', RendererStyleFlags2.DashCase);
   }
 }
