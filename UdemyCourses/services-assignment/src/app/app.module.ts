99b9d09d1da78e5682da85f7fa6563548d6825e1
RohithVenkataRamanaNanthan <rohith_venkatarama@srmuniv.edu.in>
2020-05-17 09:53:16 +0530

    service-assignment initial commit

---
+++ UdemyCourses/services-assignment/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ActiveUsersComponent } from './active-users/active-users.component';
+import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ActiveUsersComponent,
+    InactiveUsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
