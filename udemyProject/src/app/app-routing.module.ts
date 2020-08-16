1fcb1271238dfaf4833e45cd1091f4ed3ed22f62
RohithVenkataRamanaNanthan <rohith_venkatarama@srmuniv.edu.in>
2020-08-16 19:26:49 +0530

    updated app routing

--- udemyProject/src/app/app-routing.module.ts
+++ udemyProject/src/app/app-routing.module.ts
@@ -1,48 +1,51 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { RecipesComponent } from './recipes/recipes.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {RecipesComponent} from './recipes/recipes.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
+import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
+import {RecipeResolverService} from "../services/recipe-resolver.service";
 
 const appRoutes: Routes = [
-    {
+  {
+    path: '',
+    redirectTo: '/recipes',
+    pathMatch: 'full'
+  },
+  {
+    path: 'recipes',
+    children: [
+      {
         path: '',
-        redirectTo: '/recipes',
-        pathMatch: 'full'
-    },
-    {
-        path: 'recipes',
-        children: [
-            {
-                path: '',
-                component: RecipeStartComponent
-            },
-            {
-                path: 'new',
-                component: RecipeEditComponent
-            },
-            {
-                path: ':id',
-                component: RecipeDetailComponent
-            },
-            {
-                path: ':id/edit',
-                component: RecipeEditComponent
-            }
-        ],
-        component: RecipesComponent
-    },
-    {
-        path: 'shopping-list',
-        component: ShoppingListComponent
-    },
+        component: RecipeStartComponent
+      },
+      {
+        path: 'new',
+        component: RecipeEditComponent
+      },
+      {
+        path: ':id',
+        component: RecipeDetailComponent,
+        resolve: [RecipeResolverService]
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent,
+        resolve: [RecipeResolverService]
+      }
+    ],
+    component: RecipesComponent
+  },
+  {
+    path: 'shopping-list',
+    component: ShoppingListComponent
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
 
