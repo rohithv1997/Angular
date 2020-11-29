6fc70a1eda5ac279e12c8cb3d4498cc34cf65ad3
RohithVenkataRamanaNanthan <rohith_venkatarama@srmuniv.edu.in>
2020-11-29 11:27:57 +0530

    Added ServiceWorker project

---
+++ UdemyCourses/serviceworkers/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { IPost } from './ipost.model';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  posts: IPost[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  public ngOnInit(): void {
+    this.http
+      .get<IPost[]>(environment.dataUrl)
+      .subscribe(fetchedPosts => (this.posts = fetchedPosts));
+  }
+}
---
+++ UdemyCourses/serviceworkers/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
